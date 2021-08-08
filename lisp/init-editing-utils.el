9b6313896d3530f7933a80b4e37746a2472394ac
cdadar <gunbanmi@gmail.com>
2021-08-09 00:31:49 +0800

    add move-file defun

--- lisp/init-editing-utils.el
+++ lisp/init-editing-utils.el
@@ -468,5 +468,28 @@ ORIG is the advised function, which is called with its ARGS."
 
 (require 'delete-block)
 
+
+
+(defun move-file (new-location)
+  "Write this file to NEW-LOCATION, and delete the old one."
+  (interactive (list (expand-file-name
+                      (if buffer-file-name
+                          (read-file-name "Move file to: ")
+                        (read-file-name "Move file to: "
+                                        default-directory
+                                        (expand-file-name (file-name-nondirectory (buffer-name))
+                                                          default-directory))))))
+  (when (file-exists-p new-location)
+    (delete-file new-location))
+  (let ((old-location (expand-file-name (buffer-file-name))))
+    (message "old file is %s and new file is %s"
+             old-location
+             new-location)
+    (write-file new-location t)
+    (when (and old-location
+               (file-exists-p new-location)
+               (not (string-equal old-location new-location)))
+      (delete-file old-location))))
+
 (provide 'init-editing-utils)
 ;;; init-editing-utils.el ends here
