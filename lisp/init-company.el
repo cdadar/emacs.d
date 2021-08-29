a8d35ef0f8e48f46fe3b56bc7e19f610e43e37d5
cdadar <gunbanmi@gmail.com>
2021-08-29 17:38:10 +0800

    add company-tabnone config

--- lisp/init-company.el
+++ lisp/init-company.el
@@ -92,7 +92,17 @@
 
 (with-eval-after-load 'company
   (when (maybe-require-package 'company-tabnine)
-    (add-to-list 'company-backends 'company-tabnine)))
+    (add-to-list 'company-backends 'company-tabnine)
+
+    ;; The free version of TabNine is good enough,
+    ;; and below code is recommended that TabNine not always
+    ;; prompt me to purchase a paid version in a large project.
+    (defadvice company-echo-show (around disable-tabnine-upgrade-message activate)
+      (let ((company-message-func (ad-get-arg 0)))
+        (when (and company-message-func
+                   (stringp (funcall company-message-func)))
+          (unless (string-match "The free version of TabNine only indexes up to" (funcall company-message-func))
+            ad-do-it))))))
 
 (provide 'init-company)
 ;;; init-company.el ends here
