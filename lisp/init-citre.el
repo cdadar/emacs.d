91a1d0b92ced3c0e2af3d151ed6058dd5c33d16d
cdadar <gunbanmi@gmail.com>
2021-09-09 23:52:32 +0800

    fix citre is still very slow over tramp

--- lisp/init-citre.el
+++ lisp/init-citre.el
@@ -7,8 +7,15 @@
 
 (when (executable-find "/usr/local/bin/readtags")
   (when (maybe-require-package 'citre)
-    (require 'citre-config)
-    (add-hook 'prog-mode-hook 'citre-auto-enable-citre-mode)
+    (autoload 'citre-update-tags-file "citre")
+    (autoload 'citre-update-this-tags-file "citre")
+    (autoload 'citre-edit-tags-file-recipe "citre")
+    (autoload 'citre-create-tags-file "citre")
+    (autoload 'citre-jump "citre")
+    (autoload 'citre-mode "citre")
+    (autoload 'citre-peek "citre")
+    (autoload 'citre-ace-peek "citre")
+    (autoload 'citre-auto-enable-citre-mode "citre")
     (setq
      citre-readtags-program "/usr/local/bin/readtags"
      citre-ctags-program "/usr/local/bin/ctags"
@@ -17,21 +24,22 @@
      ;; See the "Create tags file" section above to know these options
      citre-use-project-root-when-creating-tags t
      citre-prompt-language-for-ctags-command t)
-    (with-eval-after-load 'citre
-      (defun citre-jump+ ()
-        (interactive)
-        (condition-case _
-            (citre-jump)
-          (error (call-interactively #'xref-find-definitions))))
-      (global-set-key (kbd "C-x c j") 'citre-jump+)
-      (global-set-key (kbd "C-x c k") 'citre-jump-back)
-      (global-set-key (kbd "C-x c p") 'citre-ace-peek)
-      (global-set-key (kbd "C-x c u") 'citre-update-this-tags-file)
+    (defun citre-jump+ ()
+      (interactive)
+      (condition-case _
+          (citre-jump)
+        (error (call-interactively #'xref-find-definitions))))
+    (global-set-key (kbd "C-x c j") 'citre-jump+)
+    (global-set-key (kbd "C-x c k") 'citre-jump-back)
+    (global-set-key (kbd "C-x c p") 'citre-ace-peek)
+    (global-set-key (kbd "C-x c u") 'citre-update-this-tags-file)
 
+    (with-eval-after-load 'citre
       (with-eval-after-load 'projectile
         (setq citre-project-root-function #'projectile-project-root))
       (with-eval-after-load 'cc-mode (require 'citre-lang-c))
-      (with-eval-after-load 'dired (require 'citre-lang-fileref)))))
+      (with-eval-after-load 'dired (require 'citre-lang-fileref))
+      (with-eval-after-load 'verilog-mode (require 'citre-lang-verilog)))))
 
 (provide 'init-citre)
 ;;; init-citre.el ends here
