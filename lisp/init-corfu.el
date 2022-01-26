cfbaa4a691e2c5673161391f12dd867f03296b15
cdadar <gunbanmi@gmail.com>
2022-01-26 18:23:58 +0800

    update corfu

--- lisp/init-corfu.el
+++ lisp/init-corfu.el
@@ -2,6 +2,18 @@
 ;;; Commentary:
 ;;; Code:
 
+;; TAB cycle if there are only few candidates
+(setq completion-cycle-threshold 3)
+
+;; Emacs 28: Hide commands in M-x which do not apply to the current mode.
+;; Corfu commands are hidden, since they are not supposed to be used via M-x.
+;; (setq read-extended-command-predicate
+;;       #'command-completion-default-include-p)
+
+;; Enable indentation+completion using the TAB key.
+;; `completion-at-point' is often bound to M-TAB.
+(setq tab-always-indent 'complete)
+
 (when (maybe-require-package 'corfu)
   (add-hook 'after-init-hook 'corfu-global-mode)
 
@@ -18,31 +30,29 @@
   ;; Enable `partial-completion' for files to allow path expansion.
   ;; You may prefer to use `initials' instead of `partial-completion'.
   (with-eval-after-load 'orderless
-    (setq completion-styles '(orderless)
+    (setq completion-styles '(orderless partial-completion)
           completion-category-defaults nil
-          completion-category-overrides '((file (styles . (partial-completion))))))
-
-
-
-  )
+          completion-category-overrides '((file (styles . (partial-completion)))))))
 
 
 (with-eval-after-load 'corfu
-  (when (maybe-require-package 'cape))
+  (when (maybe-require-package 'cape)
+    (add-hook 'after-init-hook
+              '(lambda ()
+                 (progn
+                   ;; Add `completion-at-point-functions', used by `completion-at-point'.
+                   (add-to-list 'completion-at-point-functions #'cape-file)
+                   (add-to-list 'completion-at-point-functions #'cape-tex)
+                   (add-to-list 'completion-at-point-functions #'cape-dabbrev)
+                   (add-to-list 'completion-at-point-functions #'cape-keyword)
+                   ;;(add-to-list 'completion-at-point-functions #'cape-sgml)
+                   ;;(add-to-list 'completion-at-point-functions #'cape-rfc1345)
+                   ;;(add-to-list 'completion-at-point-functions #'cape-abbrev)
+                   ;;(add-to-list 'completion-at-point-functions #'cape-ispell)
+                   ;;(add-to-list 'completion-at-point-functions #'cape-dict)
+                   ;;(add-to-list 'completion-at-point-functions #'cape-symbol)
+                   ;;(add-to-list 'completion-at-point-functions #'cape-line)
+                   )))))
 
-  (add-hook 'after-init-hook
-            '(lambda ()
-               (progn
-                 ;; Add `completion-at-point-functions', used by `completion-at-point'.
-                 (add-to-list 'completion-at-point-functions #'cape-file)
-                 (add-to-list 'completion-at-point-functions #'cape-tex)
-                 (add-to-list 'completion-at-point-functions #'cape-dabbrev)
-                 (add-to-list 'completion-at-point-functions #'cape-keyword)
-                 ;;(add-to-list 'completion-at-point-functions #'cape-sgml)
-                 ;;(add-to-list 'completion-at-point-functions #'cape-rfc1345)
-                 ;;(add-to-list 'completion-at-point-functions #'cape-abbrev)
-                 ;;(add-to-list 'completion-at-point-functions #'cape-ispell)
-                 ;;(add-to-list 'completion-at-point-functions #'cape-dict)
-                 ;;(add-to-list 'completion-at-point-functions #'cape-symbol)
-                 ;;(add-to-list 'completion-at-point-functions #'cape-line)
-                 ))))
+(provide 'init-corfu)
+;;; init-corfu.el ends here
