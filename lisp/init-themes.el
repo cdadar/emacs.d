654d2e32dce8b19829948d116286fcfb9bf2f4e9
cdadar <gunbanmi@gmail.com>
2022-03-23 23:29:00 +0800

    update modus-themes not after load

--- lisp/init-themes.el
+++ lisp/init-themes.el
@@ -4,16 +4,15 @@
 
 (require-package 'modus-themes)
 
-;; Toggle between light and dark
-(with-eval-after-load 'modus-themes
-  ;; Add all your customizations prior to loading the themes
-  (setq modus-themes-italic-constructs t
-        modus-themes-bold-constructs nil
-        modus-themes-region '(bg-only no-extend))
 
-  ;; Load the theme files before enabling a theme
-  (modus-themes-load-themes)
-  (modus-themes-load-vivendi))
+;; Add all your customizations prior to loading the themes
+(setq modus-themes-italic-constructs t
+      modus-themes-bold-constructs nil
+      modus-themes-region '(bg-only no-extend))
+
+;; Load the theme files before enabling a theme
+(modus-themes-load-themes)
+(modus-themes-load-vivendi)
 
 (define-key global-map (kbd "<f5>") #'modus-themes-toggle)
 
