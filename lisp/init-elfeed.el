9955427915d8e3253d5c8586805af54c78fa25a7
cdadar <gunbanmi@gmail.com>
2022-06-24 16:23:28 +0800

    update elfeed with elfeed-org

--- lisp/init-elfeed.el
+++ lisp/init-elfeed.el
@@ -2,17 +2,27 @@
 ;;; Commentary:
 ;;; Code:
 
-;; -*- lexical-binding: t -*-
+
 
 (use-package elfeed
+  :commands (elfeed-org)
   :bind
-  (("C-x w" . elfeed)
+  (("C-x w" . (lambda () (interactive)  (progn (elfeed-org) (elfeed))))
    :map elfeed-search-mode-map
    ("q" . bjm/elfeed-save-db-and-bury))
+  :init
+  (setq elfeed-use-curl 't)
   :config
   (progn
-    (setq elfeed-use-curl 't)
-    (setq rmh-elfeed-org-auto-ignore-invalid-feeds t)
+    (use-package elfeed-org
+      :init
+      (progn
+        (setq rmh-elfeed-org-auto-ignore-invalid-feeds t)
+        (setq rmh-elfeed-org-files (list rmh-elfeed-org-file)))
+      :config
+      (elfeed-org))
+
+
     ;;functions to support syncing .elfeed between machines
     ;;makes sure elfeed reads index from disk before launching
     (defun bjm/elfeed-load-db-and-open ()
@@ -27,11 +37,7 @@
       "Wrapper to save the elfeed db to disk before burying buffer"
       (interactive)
       (elfeed-db-save)
-      (quit-window))
-    (use-package elfeed-org
-      :config
-      (elfeed-org))))
-
+      (quit-window))))
 
 
 
--- lisp/init-local.el
+++ lisp/init-local.el
@@ -23,7 +23,7 @@
 (setq org-mobile-inbox-for-pull "~/MobileOrg/index.org")
 (setq org-mobile-directory "~/MobileOrg")
 
-(setq rmh-elfeed-org-file "~/org-mode/elfeed.org")
+
 
 (setq org-roam-directory "~/org-mode/roam")
 
@@ -48,35 +48,31 @@
 (setq erc-autojoin-channels-alist
       '(("freenode.net" "#lisp" "#opensuse-cn" "#fedora-zh" "#linuxba" "#emacs" "#java" "#javascript" "#python" "#python.cn" "#ruby")))
 
-(with-eval-after-load 'org-ref
-  (setq reftex-default-bibliography '("~/Nutstore Files/Nutstore/bibliography/references.bib"))
+(setq reftex-default-bibliography '("~/Nutstore Files/Nutstore/bibliography/references.bib"))
 
-  ;; see org-ref for use of these variables
-  (setq org-ref-bibliography-notes "~/Nutstore Files/Nutstore/bibliography/notes.org"
-        org-ref-default-bibliography '("~/Nutstore Files/Nutstore/bibliography/references.bib")
-        org-ref-pdf-directory "~/Nutstore Files/Nutstore/bibliography/bibtex-pdfs/")
+;; see org-ref for use of these variables
+(setq org-ref-bibliography-notes "~/Nutstore Files/Nutstore/bibliography/notes.org"
+      org-ref-default-bibliography '("~/Nutstore Files/Nutstore/bibliography/references.bib")
+      org-ref-pdf-directory "~/Nutstore Files/Nutstore/bibliography/bibtex-pdfs/")
 
-  (setq bibtex-completion-bibliography "~/Nutstore Files/Nutstore/bibliography/references.bib"
-        bibtex-completion-library-path "~/Nutstore Files/Nutstore/bibliography/bibtex-pdfs"
-        bibtex-completion-notes-path "~/Nutstore Files/Nutstore/bibliography/helm-bibtex-notes")
+(setq bibtex-completion-bibliography "~/Nutstore Files/Nutstore/bibliography/references.bib"
+      bibtex-completion-library-path "~/Nutstore Files/Nutstore/bibliography/bibtex-pdfs"
+      bibtex-completion-notes-path "~/Nutstore Files/Nutstore/bibliography/helm-bibtex-notes")
 
-  ;; open pdf with system pdf viewer (works on mac)
-  (setq bibtex-completion-pdf-open-function
-        (lambda (fpath)
-          (start-process "open" "*open*" "open" fpath)))
+;; open pdf with system pdf viewer (works on mac)
+(setq bibtex-completion-pdf-open-function
+      (lambda (fpath)
+        (start-process "open" "*open*" "open" fpath)))
 
-  ;; alternative
-  ;; (setq bibtex-completion-pdf-open-function 'org-open-file)
+;; alternative
+;; (setq bibtex-completion-pdf-open-function 'org-open-file)
 
-  )
 
-(with-eval-after-load 'org-roam
-  (setq org-roam-directory "~/org-mode/roam"))
 
-(with-eval-after-load 'elfeed
-  (setq elfeed-db-directory "~/Nutstore Files/Nutstore/.elfeed"))
-(with-eval-after-load 'elfeed-org
-  (setq rmh-elfeed-org-files (list rmh-elfeed-org-file)))
+(setq org-roam-directory "~/org-mode/roam")
+(setq elfeed-db-directory "~/Nutstore Files/Nutstore/.elfeed")
+(setq rmh-elfeed-org-file "~/org-mode/elfeed.org")
+
 
 (custom-set-variables
  '(markdown-command "pandoc")
