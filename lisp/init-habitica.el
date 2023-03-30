adea905b41b1f1f6254869cb9ce92d4881d9a9a9
cdadar <gunbanmi@gmail.com>
2023-03-30 10:30:31 +0800

    add habitica.el

---
+++ lisp/init-habitica.el
@@ -0,0 +1,11 @@
+;;; init-habitica.el --- habitica.com -*- lexical-binding: t -*-
+;;; Commentary:
+;;; Code:
+
+(use-package habitica
+  :config
+  (setq habitica-turn-on-highlighting t)
+  (setq habitica-show-streak t))
+
+(provide 'init-habitica)
+;;; init-habitica.el ends here
