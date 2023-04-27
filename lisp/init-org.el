a0d68b0f4232ff78743ccf2b62c74479f72815cf
cdadar <gunbanmi@gmail.com>
2023-04-28 00:31:17 +0800

    feat: update ex-epub

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -647,32 +647,32 @@ typical word processor."
             "xelatex -interaction nonstopmode -output-directory %o %f"
             "xelatex -interaction nonstopmode -output-directory %o %f")) ;; org v8
     ;; }}
+
+    (use-package ox-epub)
     
-
-
     (org-babel-do-load-languages
      'org-babel-load-languages
      (seq-filter
       (lambda (pair)
-      (locate-library (concat "ob-" (symbol-name (car pair)))))
-     '((R . t)
-       (ditaa . t)
-       (dot . t)
-       (emacs-lisp . t)
-       (gnuplot . t)
-       (haskell . nil)
-       (latex . t)
-       (ledger . t)
-       (ocaml . nil)
-       (octave . t)
-       (plantuml . t)
-       (python . t)
-       (ruby . t)
-       (screen . nil)
-       (sh . t) ;; obsolete
-       (shell . t)
-       (sql . t)
-       (sqlite . t))))
+        (locate-library (concat "ob-" (symbol-name (car pair)))))
+      '((R . t)
+        (ditaa . t)
+        (dot . t)
+        (emacs-lisp . t)
+        (gnuplot . t)
+        (haskell . nil)
+        (latex . t)
+        (ledger . t)
+        (ocaml . nil)
+        (octave . t)
+        (plantuml . t)
+        (python . t)
+        (ruby . t)
+        (screen . nil)
+        (sh . t) ;; obsolete
+        (shell . t)
+        (sql . t)
+        (sqlite . t))))
 
     (require 'org-crypt)
     ;; org-mode 設定
