4761d9e3f268c34e4be2cfefb6c36e111c9fdecc
cdadar <gunbanmi@gmail.com>
2023-07-23 21:09:41 +0800

    update ledger config and  add snippets

--- lisp/init-ledger.el
+++ lisp/init-ledger.el
@@ -15,13 +15,13 @@
         ledger-use-iso-dates nil
         ledger-mode-should-check-version nil)
 
-  (set ledger-reports
-       '(("当月收支分类统计明细" "%(binary) [[ledger-mode-flags]] -f %(ledger-file) reg ^Expenses ^Incomes -M -X CNY --begin '2023-07-01'")
-         ("每月收支分类明细" "%(binary) [[ledger-mode-flags]] -f %(ledger-file) reg ^Expenses ^Incomes -M -X CNY")
-         ("bal" "%(binary) -f %(ledger-file) bal")
-         ("reg" "%(binary) -f %(ledger-file) reg")
-         ("payee" "%(binary) -f %(ledger-file) reg @%(payee)")
-         ("account" "%(binary) -f %(ledger-file) reg %(account)")))
+  ;; (set ledger-reports
+  ;;      '(("当月收支分类统计明细" "%(binary) [[ledger-mode-flags]] -f %(ledger-file) reg ^Expenses ^Incomes -M -X CNY --begin '2023-07-01'")
+  ;;        ("每月收支分类明细" "%(binary) [[ledger-mode-flags]] -f %(ledger-file) reg ^Expenses ^Incomes -M -X CNY")
+  ;;        ("bal" "%(binary) -f %(ledger-file) bal")
+  ;;        ("reg" "%(binary) -f %(ledger-file) reg")
+  ;;        ("payee" "%(binary) -f %(ledger-file) reg @%(payee)")
+  ;;        ("account" "%(binary) -f %(ledger-file) reg %(account)")))
 
   (when (memq window-system '(mac ns))
     (exec-path-from-shell-copy-env "LEDGER_FILE")
