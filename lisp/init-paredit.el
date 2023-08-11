797ae59e10b9134a08123ce893fde3485a19144c
Steve Purcell <steve@sanityinc.com>
2023-08-11 19:22:53 +0200

    Remove defunct local patch for dash-at-point

--- lisp/init-paredit.el
+++ lisp/init-paredit.el
@@ -14,10 +14,9 @@
 
   ;; Suppress certain paredit keybindings to avoid clashes, including
   ;; my global binding of M-?
-
   (dolist (binding '("RET" "C-<left>" "C-<right>" "C-M-<left>" "C-M-<right>" "M-s" "M-?"))
     (define-key paredit-mode-map (read-kbd-macro binding) nil))
-  (define-key paredit-mode-map (kbd "M-<up>") 'paredit-splice-sexp-killing-backward))
+  (define-key paredit-mode-map (kbd "M-<up>") 'paredit-splice-sexp-killing-backward)
 
   ;; Use paredit in the minibuffer
   ;; TODO: break out into separate package
