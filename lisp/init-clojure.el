f3b8a5de10404a76c4a818d8db505b6f940ecbf2
Steve Purcell <steve@sanityinc.com>
2023-08-13 18:42:31 +0200

    No subword-mode in clojure-mode

---
+++ lisp/init-clojure.el
@@ -0,0 +1,18 @@
+;;; init-clojure.el --- Clojure support -*- lexical-binding: t -*-
+;;; Commentary:
+;;; Code:
+
+;; See also init-clojure-cider.el
+(use-package clojure-mode
+  :config
+  (dolist (m '(clojure-mode-hook clojure-ts-mode-hook))
+      (add-hook m 'sanityinc/lisp-setup)))
+
+(use-package cljsbuild-mode
+  :after clojure-mode)
+
+(use-package elein
+  :after clojure-mode)
+
+(provide 'init-clojure)
+;;; init-clojure.el ends here
