43ce36523cba7e9b7e3a053c9f4eff6724edcf6d
Steve Purcell <steve@sanityinc.com>
2023-10-29 15:31:27 +0000

    Better setup for nix-ts-mode

--- lisp/init-nix.el
+++ lisp/init-nix.el
@@ -2,15 +2,28 @@
 ;;; Commentary:
 ;;; Code:
 
-(use-package nix-mode
+
+
+(use-package nix-ts-mode
+  :config
+  (defun sanityinc/set-nix-ts-auto-mode ()
+        (when (and (fboundp 'treesit-ready-p)
+                   (treesit-ready-p 'nix)
+                   (fboundp 'nix-ts-mode))
+          (add-to-list 'auto-mode-alist '("\\.nix\\'" . nix-ts-mode))))
+  (add-hook 'after-init-hook 'sanityinc/set-nix-ts-auto-mode)
+  (use-package nix-mode
   :config
   (with-eval-after-load 'eglot
-    (add-to-list 'eglot-server-programs '((nix-mode) . ("nil")))))
+    (add-to-list 'eglot-server-programs '((nix-mode) . ("nil"))))))
+
 (use-package nixpkgs-fmt
   :after (:any nix-ts-mode nix-mode))
 (use-package nix-sandbox)
 (use-package nix-buffer)
 (use-package nixos-options)
+
+
 
 (provide 'init-nix)
 ;;; init-nix.el ends here
