a8a2a9993b468b3eb57840ce71e7b0f423ea22b1
chens <chens@fangde.com>
2024-01-03 18:14:22 +0800

    update sly with use-package

--- lisp/init-sly.el
+++ lisp/init-sly.el
@@ -2,35 +2,32 @@
 ;;; Commentary:
 ;;; Code:
 
-(require-package 'sly)
-(require-package 'sly-asdf)
-(require-package 'sly-macrostep)
-(require-package 'sly-repl-ansi-color)
 
-
-;;; Lisp buffers
 
-(with-eval-after-load 'sly
+(use-package sly
+  :config
+  ;;; Lisp buffers
   (setq sly-protocol-version 'ignore)
   (setq sly-net-coding-system 'utf-8-unix)
   (let ((features '(sly-fancy)))
     ;; (when (require 'sly-company nil t)
     ;;   (push 'sly-company features))
-    (sly-setup features)))
+    (sly-setup features))
+  ;;; REPL
+  (with-eval-after-load 'sly-repl
+    ;; Stop SLY's REPL from grabbing DEL, which is annoying when backspacing over a '('
+    (with-eval-after-load 'paredit
+      (define-key sly-repl-mode-map (read-kbd-macro paredit-backward-delete-key) nil))
 
-
-;;; REPL
+    ;; Bind TAB to `indent-for-tab-command', as in regular Sly buffers.
+    (define-key sly-repl-mode-map (kbd "TAB") 'indent-for-tab-command)
 
-(with-eval-after-load 'sly-repl
-  ;; Stop SLY's REPL from grabbing DEL, which is annoying when backspacing over a '('
-  (with-eval-after-load 'paredit
-    (define-key sly-repl-mode-map (read-kbd-macro paredit-backward-delete-key) nil))
+    (add-hook 'sly-repl-mode-hook 'sanityinc/lisp-setup)))
 
-  ;; Bind TAB to `indent-for-tab-command', as in regular Sly buffers.
-  (define-key sly-repl-mode-map (kbd "TAB") 'indent-for-tab-command)
-
-  (add-hook 'sly-repl-mode-hook 'sanityinc/lisp-setup))
-
+(use-package sly-asdf)
+(use-package sly-quicklisp)
+(use-package sly-macrostep)
+(use-package sly-repl-ansi-color)
 
 (provide 'init-sly)
 ;;; init-sly.el ends here
