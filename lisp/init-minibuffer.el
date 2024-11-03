5c09edc92e64b237c2f449451bdc575f9d7c06a8
Steve Purcell <steve@sanityinc.com>
2024-11-03 09:43:03 +0000

    Fix use of prefix arg in consult-ripgrep wrapper
    Fixes #866
    
    Thanks @wangdiqi

--- lisp/init-minibuffer.el
+++ lisp/init-minibuffer.el
@@ -157,7 +157,7 @@
   ;; :preview-key on a per-command basis using the `consult-customize' macro.
   (when (and (executable-find "rg"))
     (defun sanityinc/consult-ripgrep-at-point (&optional dir initial)
-      (interactive (list prefix-arg (when-let ((s (symbol-at-point)))
+      (interactive (list current-prefix-arg (when-let ((s (symbol-at-point)))
                                       (symbol-name s))))
       (consult-ripgrep dir initial)))
 
