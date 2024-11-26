9b9e112651a38e7f60efd56a4d91226bac045329
cdadar <gunbanmi@gmail.com>
2024-11-26 15:55:02 +0800

    update disable zig-mode

--- lisp/init-zig.el
+++ lisp/init-zig.el
@@ -2,13 +2,15 @@
 ;;; Commentary:
 ;;; Code:
 
-(if (and (maybe-require-package 'zig-ts-mode)
-         (fboundp 'treesit-ready-p) (treesit-ready-p 'zig))
-    (progn
-      (add-to-list 'auto-mode-alist '("\\.\\(zig\\|zon\\)\\'" . zig-ts-mode))
+(use-package zig-ts-mode
+  :if (and  (fboundp 'treesit-ready-p) (treesit-ready-p 'zig))
+  :mode ("\\.\\(zig\\|zon\\)\\'" . zig-ts-mode)
+  :config
+  (progn
       (with-eval-after-load 'eglot
-        (add-to-list 'eglot-server-programs '(zig-ts-mode . ("zls")))))
-  (require-package 'zig-mode))
+        (add-to-list 'eglot-server-programs '(zig-ts-mode . ("zls"))))))
+
+(use-package zig-mode)
 
 
 (provide 'init-zig)
