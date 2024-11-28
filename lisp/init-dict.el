d654debfdd07f5ee6a05b1a3d8d384ab385e395b
cdadar <gunbanmi@gmail.com>
2024-11-28 11:20:17 +0800

    drop youdao-dict

--- lisp/init-dict.el
+++ lisp/init-dict.el
@@ -1,12 +1,8 @@
-;;; init-dict.el --- youdao dictionary  -*- lexical-binding: t -*-
+;;; init-dict.el --- dictionary  -*- lexical-binding: t -*-
 ;;; Commentary:
 ;;; Code:
 
 ;; -*- lexical-binding: t -*-
-(use-package youdao-dictionary
-  :config
-  (setq url-automatic-caching t)
-  :bind
-  (("C-c C-y" . youdao-dictionary-search-at-point)))
+
 
 (provide 'init-dict)
