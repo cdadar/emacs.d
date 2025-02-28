268b417fb223ed1e04bf4283368b8848d7428b8d
cdadar <gunbanmi@gmail.com>
2025-02-28 16:37:12 +0800

    replace cape-prefix-map to binds cape commands

--- lisp/init-corfu.el
+++ lisp/init-corfu.el
@@ -82,23 +82,7 @@
 (use-package cape
   ;; Bind dedicated completion commands
   ;; Alternative prefix keys: C-c p, M-p, M-+, ...
-  :bind (("C-c p p" . completion-at-point) ;; capf
-         ("C-c p t" . complete-tag)        ;; etags
-         ("C-c p d" . cape-dabbrev)        ;; or dabbrev-completion
-         ("C-c p h" . cape-history)
-         ("C-c p f" . cape-file)
-         ("C-c p k" . cape-keyword)
-         ("C-c p s" . cape-elisp-symbol)
-         ("C-c p e" . cape-elisp-block)
-         ("C-c p a" . cape-abbrev)
-         ("C-c p l" . cape-line)
-         ("C-c p w" . cape-dict)
-         ("C-c p :" . cape-emoji)
-         ("C-c p \\" . cape-tex)
-         ("C-c p _" . cape-tex)
-         ("C-c p ^" . cape-tex)
-         ("C-c p &" . cape-sgml)
-         ("C-c p r" . cape-rfc1345))
+  :bind (("C-c p" . cape-prefix-map))
   :init
   (setq cape-dabbrev-min-length 3)
  ;; Add to the global default value of `completion-at-point-functions' which is
