ec80397f49613432bdc70686371876f4cb08accd
cdadar <gunbanmi@gmail.com>
2026-02-25 00:05:15 +0800

    update ai config for ai-code

--- lisp/init-ai.el
+++ lisp/init-ai.el
@@ -122,23 +122,30 @@
     (aider-magit-setup-transients) ;; add aider magit function to magit menu
     ;; auto revert buffer
     (global-auto-revert-mode 1)
-    (auto-revert-mode 1))
+    (auto-revert-mode 1)))
 
 
-  (use-package ai-code
-    :config
-    ;; use codex as backend, other options are 'gemini, 'github-copilot-cli, 'opencode, 'grok, 'claude-code-ide, 'claude-code, 'cursor
-    (ai-code-set-backend 'codex)
-    ;; Enable global keybinding for the main menu
-    (global-set-key (kbd "C-c a") #'ai-code-menu)
-    ;; Optional: Use eat if you prefer, by default it is vterm
-    ;; (setq ai-code-backends-infra-terminal-backend 'eat) ;; for openai codex, github copilot cli, opencode, grok, cursor-cli; for claude-code-ide.el, you can check their config
-    ;; Optional: Turn on auto-revert buffer, so that the AI code change automatically appears in the buffer
-    (global-auto-revert-mode 1)
-    (setq auto-revert-interval 1) ;; set to 1 second for faster update
-    ;; Optional: Set up Magit integration for AI commands in Magit popups
-    (with-eval-after-load 'magit
-      (ai-code-magit-setup-transients))))
+(use-package vterm
+  :ensure t
+  :config
+  (setq vterm-shell "/bin/zsh"))
+
+
+(use-package ai-code
+  :after (vterm)
+  :config
+  ;; use codex as backend, other options are 'gemini, 'github-copilot-cli, 'opencode, 'grok, 'claude-code-ide, 'claude-code, 'cursor
+  (ai-code-set-backend 'codex)
+  ;; Enable global keybinding for the main menu
+  (global-set-key (kbd "C-c a") #'ai-code-menu)
+  ;; Optional: Use eat if you prefer, by default it is vterm
+  ;; (setq ai-code-backends-infra-terminal-backend 'eat) ;; for openai codex, github copilot cli, opencode, grok, cursor-cli; for claude-code-ide.el, you can check their config
+  ;; Optional: Turn on auto-revert buffer, so that the AI code change automatically appears in the buffer
+  (global-auto-revert-mode 1)
+  (setq auto-revert-interval 1) ;; set to 1 second for faster update
+  ;; Optional: Set up Magit integration for AI commands in Magit popups
+  (with-eval-after-load 'magit
+    (ai-code-magit-setup-transients)))
 
 
 (use-package eca)
