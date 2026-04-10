9efdffbedbd7b53f623621c1f8e909e1fdcf9274
cdadar <gunbanmi@gmail.com>
2026-04-10 23:41:41 +0800

    disable mouse-wheel-text-scale

--- lisp/init-fonts.el
+++ lisp/init-fonts.el
@@ -16,6 +16,21 @@
   :config
   (cnfonts-mode 1))
 
+
+
+(use-package mouse
+  :ensure nil
+  :init
+  (setq mouse-wheel-scroll-amount '(1 ((shift) . 1))
+        mouse-wheel-progressive-speed nil
+        mouse-wheel-follow-mouse t)
+
+  :bind
+  (("<C-mouse-4>" . nil)
+   ("<C-mouse-5>" . nil)
+   ("<C-wheel-up>" . nil)
+   ("<C-wheel-down>" . nil)))
+
 (provide 'init-fonts)
 
 ;;; init-fonts.el ends here
