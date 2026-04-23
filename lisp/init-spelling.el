a9376ef56133bfd6d4f334b7a9af53af1e0990ab
cdadar <gunbanmi@gmail.com>
2026-04-23 23:18:45 +0800

    refactor: modernize spelling configuration

--- lisp/init-spelling.el
+++ lisp/init-spelling.el
@@ -4,7 +4,7 @@
 
 ;; {{ flyspell setup for web-mode
 (defun web-mode-flyspell-verify ()
-  (let* ((f (get-text-property (- (point) 1) 'face))
+  (let* ((f (get-text-property (max (point-min) (1- (point))) 'face))
          rlt)
     (cond
      ;; Check the words with these font faces, possibly.
@@ -27,10 +27,10 @@
      ;; check attribute value under certain conditions
      ((memq f '(web-mode-html-attr-value-face))
       (save-excursion
-        (search-backward-regexp "=['\"]" (line-beginning-position) t)
-        (backward-char)
-        (setq rlt (string-match "^\\(value\\|class\\|ng[A-Za-z0-9-]*\\)$"
-                                (thing-at-point 'symbol)))))
+        (when (search-backward-regexp "=['\"]" (line-beginning-position) t)
+          (backward-char)
+          (setq rlt (string-match "^\\(value\\|class\\|ng[A-Za-z0-9-]*\\)$"
+                                  (thing-at-point 'symbol))))))
      ;; finalize the blacklist
      (t
       (setq rlt nil)))
@@ -40,7 +40,7 @@
 
 ;; {{ flyspell setup for js2-mode
 (defun js-flyspell-verify ()
-  (let* ((f (get-text-property (- (point) 1) 'face)))
+  (let* ((f (get-text-property (max (point-min) (1- (point))) 'face)))
     ;; *whitelist*
     ;; only words with following font face will be checked
     (memq f '(js2-function-call
@@ -71,8 +71,8 @@ Please note RUN-TOGETHER will make aspell less capable. So it should only be use
        ((string-match "aspell$" ispell-program-name)
         ;; force the English dictionary, support Camel Case spelling check (tested with aspell 0.6)
         (setq args (list "--sug-mode=ultra" "--lang=en_US"))
-        (if run-together
-            (setq args (append args '("--run-together" "--run-together-limit=16" "--run-together-min=2")))))
+        (when run-together
+          (setq args (append args '("--run-together" "--run-together-limit=16" "--run-together-min=2")))))
        ((string-match "hunspell$" ispell-program-name)
         (setq args nil))))
     args))
@@ -98,9 +98,7 @@ Please note RUN-TOGETHER will make aspell less capable. So it should only be use
 
 (use-package ispell
   :ensure nil
-  :init
-  (require 'ispell)
-  (setq-default ispell-extra-args (flyspell-detect-ispell-args t))
+  :demand t
   :config
   (cond
    ((executable-find "aspell")
@@ -113,26 +111,25 @@ Please note RUN-TOGETHER will make aspell less capable. So it should only be use
    (t (setq ispell-program-name nil)
       (message "You need install either aspell or hunspell for ispell")))
 
-  (defadvice ispell-word (around my-ispell-word activate)
+  (setq-default ispell-extra-args (flyspell-detect-ispell-args t))
+
+  (defun cdadar/with-standard-ispell-args (orig-fun &rest args)
+    "Run ORIG-FUN with standard ispell arguments, then restore custom args."
     (let ((old-ispell-extra-args ispell-extra-args))
       (ispell-kill-ispell t)
-      ;; use emacs original arguments
-      (setq ispell-extra-args (flyspell-detect-ispell-args))
-      ad-do-it
-      ;; restore our own ispell arguments
-      (setq ispell-extra-args old-ispell-extra-args)
-      (ispell-kill-ispell t)
-      ))
+      (unwind-protect
+          (progn
+            ;; use emacs original arguments
+            (setq ispell-extra-args (flyspell-detect-ispell-args))
+            (apply orig-fun args))
+        ;; restore our own ispell arguments
+        (setq ispell-extra-args old-ispell-extra-args)
+        (ispell-kill-ispell t))))
 
-  (defadvice flyspell-auto-correct-word (around my-flyspell-auto-correct-word activate)
-    (let* ((old-ispell-extra-args ispell-extra-args))
-      (ispell-kill-ispell t)
-      ;; use emacs original arguments
-      (setq ispell-extra-args (flyspell-detect-ispell-args))
-      ad-do-it
-      ;; restore our own ispell arguments
-      (setq ispell-extra-args old-ispell-extra-args)
-      (ispell-kill-ispell t))))
+  (unless (advice-member-p #'cdadar/with-standard-ispell-args 'ispell-word)
+    (advice-add 'ispell-word :around #'cdadar/with-standard-ispell-args))
+  (unless (advice-member-p #'cdadar/with-standard-ispell-args 'flyspell-auto-correct-word)
+    (advice-add 'flyspell-auto-correct-word :around #'cdadar/with-standard-ispell-args)))
 ;; }}
 
 ;; {{ flyspell configuration
@@ -140,17 +137,27 @@ Please note RUN-TOGETHER will make aspell less capable. So it should only be use
   ;; Turn off RUN-TOGETHER option when spell check text-mode
   (setq-local ispell-extra-args (flyspell-detect-ispell-args)))
 
+(defun cdadar/enable-flyspell-prog-mode-conditionally ()
+  "Enable `flyspell-prog-mode' when spell checking is available."
+  (when (can-enable-flyspell-mode)
+    (flyspell-prog-mode)))
+
 (use-package flyspell
   :ensure nil
   :bind (("C-c s" . flyspell-auto-correct-word)
          :map flyspell-mode-map
          ("C-." . nil)
          ("C-;" . nil))
-  :hook (text-mode . text-mode-hook-setup)
+  :hook ((text-mode . text-mode-hook-setup)
+         (prog-mode . cdadar/enable-flyspell-prog-mode-conditionally))
   :custom
   (flyspell-issue-message-flag nil)
   :config
-  (add-to-list 'flyspell-prog-text-faces 'nxml-text-face))
+  (add-to-list 'flyspell-prog-text-faces 'nxml-text-face)
+  (unless (advice-member-p #'flyspell-highlight-incorrect-region-hack
+                           'flyspell-highlight-incorrect-region)
+    (advice-add 'flyspell-highlight-incorrect-region
+                :around #'flyspell-highlight-incorrect-region-hack)))
 
 ;; Add auto spell-checking in comments for all programming language modes
 ;; if and only if there is enough memory
@@ -161,12 +168,11 @@ Please note RUN-TOGETHER will make aspell less capable. So it should only be use
        (executable-find ispell-program-name)))
 
 (defun enable-flyspell-mode-conditionally ()
-  (if (can-enable-flyspell-mode)
-      (flyspell-mode 1)))
+  (when (can-enable-flyspell-mode)
+    (flyspell-mode 1)))
 
 ;; turn on flyspell-mode for programming languages
-(if (can-enable-flyspell-mode)
-    (add-hook 'prog-mode-hook 'flyspell-prog-mode))
+;; See the `flyspell' use-package hook above.
 
 ;; I don't use flyspell in text-mode because I often write Chinese.
 ;; I'd rather manually spell check the English text
@@ -174,18 +180,22 @@ Please note RUN-TOGETHER will make aspell less capable. So it should only be use
 ;; {{ avoid spell-checking doublon (double word) in certain major modes
 (defvar flyspell-check-doublon t
   "Check doublon (double word) when calling `flyspell-highlight-incorrect-region'.")
+
 (make-variable-buffer-local 'flyspell-check-doublon)
 
-(defadvice flyspell-highlight-incorrect-region (around flyspell-highlight-incorrect-region-hack activate)
-  (if (or flyspell-check-doublon (not (eq 'doublon (ad-get-arg 2))))
-      ad-do-it))
+(defun flyspell-highlight-incorrect-region-hack (orig-fun &rest args)
+  "Skip doublon highlighting when `flyspell-check-doublon' is nil."
+  (when (or flyspell-check-doublon (not (eq 'doublon (nth 2 args))))
+    (apply orig-fun args)))
 ;; }}
 
 (defun my-clean-aspell-dict ()
   "Clean ~/.aspell.pws (dictionary used by aspell)."
   (interactive)
   (let* ((dict (file-truename "~/.aspell.en.pws"))
-         (lines (read-lines dict))
+         (lines (with-temp-buffer
+                  (insert-file-contents dict)
+                  (split-string (buffer-string) "\n" t)))
          ;; sort words
          (aspell-words (sort (cdr lines) 'string<)))
     (with-temp-file dict
@@ -193,9 +203,6 @@ Please note RUN-TOGETHER will make aspell less capable. So it should only be use
                       "personal_ws-1.1 en"
                       (length aspell-words)
                       (mapconcat 'identity aspell-words "\n"))))))
-
-(when (executable-find ispell-program-name)
-  (add-hook 'prog-mode-hook 'flyspell-prog-mode))
 
 (provide 'init-spelling)
 ;;; init-spelling.el ends here
