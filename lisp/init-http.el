ef7cac3219d3cb3e4fc24c1b9526d58c60068e71
cdadar <gunbanmi@gmail.com>
2026-04-25 21:53:40 +0800

    refactor: tidy http client package setup

--- lisp/init-http.el
+++ lisp/init-http.el
@@ -3,23 +3,26 @@
 ;;; Code:
 
 (use-package httprepl
+  :preface
+  (defun sanityinc/httprepl-render-image (buffer)
+    "Render HTTPREPL image response in BUFFER using `image-mode'."
+    (with-current-buffer buffer
+      (image-mode)
+      buffer))
   :config
   (push '("image" . image) httprepl-content-type-alist)
-  (push '(image . ((lambda (b) (with-current-buffer b
-                                 (image-mode)
-                                 b))))
+  (push `(image . (,#'sanityinc/httprepl-render-image))
         httprepl-content-type-middleware-alist))
 
-
 (use-package restclient
-  :config
-  (add-auto-mode 'restclient-mode "\\.rest\\'")
+  :preface
   (defun sanityinc/restclient ()
     "Work with `rest' in the *restclient* buffer."
     (interactive)
     (with-current-buffer (get-buffer-create "*restclient*")
       (restclient-mode)
-      (pop-to-buffer (current-buffer)))))
+      (pop-to-buffer (current-buffer))))
+  :mode ("\\.rest\\'" . restclient-mode))
 
 (provide 'init-http)
 ;;; init-http.el ends here
