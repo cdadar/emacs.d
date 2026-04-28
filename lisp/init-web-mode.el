91a5bc1d35f22e8169ebf42d755c8215d6c88211
cdadar <gunbanmi@gmail.com>
2026-04-29 00:54:59 +0800

    refactor: tidy web-mode configuration

--- lisp/init-web-mode.el
+++ lisp/init-web-mode.el
@@ -29,49 +29,46 @@
    ("\\.xul?\\'" . web-mode)
    ("\\.eex?\\'" . web-mode)
    ("\\.xml?\\'" . web-mode)
-   ("\\.\\(jst\\|ejs\\|rhtml\\)\\(\\.erb\\)?\\'"  . web-mode) ; ruby
-   )
+   ("\\.\\(jst\\|ejs\\|rhtml\\)\\(\\.erb\\)?\\'" . web-mode)) ; ruby
+  :custom
+  (web-mode-enable-auto-closing t)      ; enable auto close tag in text-mode
+  (web-mode-enable-auto-pairing t)
+  (web-mode-enable-css-colorization t)
+  (web-mode-markup-indent-offset 2)
+  (web-mode-code-indent-offset 2)
+  (web-mode-css-indent-offset 2)
+  (web-mode-enable-auto-expanding t)
   :config
   ;; make org-mode export fail, I use evil and evil-matchit
   ;; to select text, so expand-region.el is not used
-
-  (remove-hook 'web-mode-hook 'er/add-web-mode-expansions)
-  (setq web-mode-enable-auto-closing t) ; enable auto close tag in text-mode
-  (setq web-mode-enable-auto-pairing t)
-  (setq web-mode-enable-css-colorization t)
-  (setq web-mode-markup-indent-offset 2)
-  (setq web-mode-code-indent-offset 2)
-  (setq web-mode-css-indent-offset 2)
-  (setq web-mode-enable-auto-expanding t)
+  (remove-hook 'web-mode-hook #'er/add-web-mode-expansions)
   (setq web-mode-imenu-regexp-list
         '(("<\\(h[1-9]\\)\\([^>]*\\)>\\([^<]*\\)" 1 3 ">" nil)
           ("^[ \t]*<\\([@a-z]+\\)[^>]*>? *$" 1 " id=\"\\([a-zA-Z0-9_]+\\)\"" "#" ">")
           ("^[ \t]*<\\(@[a-z.]+\\)[^>]*>? *$" 1 " contentId=\"\\([a-zA-Z0-9_]+\\)\"" "=" ">")
           ;; angular imenu
-          (" \\(ng-[a-z]*\\)=\"\\([^\"]+\\)" 1 2 "=")))
-  (use-package instant-rename-tag
-    :vc(:url"https://github.com/manateelazycat/instant-rename-tag" :rev :newest)))
+          (" \\(ng-[a-z]*\\)=\"\\([^\"]+\\)" 1 2 "="))))
 
+(use-package instant-rename-tag
+  :after web-mode
+  :vc (:url "https://github.com/manateelazycat/instant-rename-tag" :rev :newest))
 
-(when (executable-find "prettier")
-  (use-package reformatter
-    :config
-    (reformatter-define prettier-css
-      :program "prettier"
-      :args '("--parser=css"))
-             ;;;###autoload (autoload 'prettier-html-on-save-mode "prettier-html" nil t)
-    (reformatter-define prettier-html
-      :program "prettier"
-      :args '("--parser=html"))
-    (add-hook 'web-mode-hook 'prettier-html-on-save-mode)))
-
+(use-package reformatter
+  :if (executable-find "prettier")
+  :demand t
+  :config
+  (reformatter-define prettier-css
+    :program "prettier"
+    :args '("--parser=css"))
+  (reformatter-define prettier-html
+    :program "prettier"
+    :args '("--parser=html"))
+  (add-hook 'web-mode-hook #'prettier-html-on-save-mode)
+  (add-hook 'ng2-html-mode-hook #'prettier-html-on-save-mode))
 
 (use-package ng2-mode
-  :mode (("*\\.{component|service|pipe|directive|guard|module}\\.ts\\'" . ng2-mode)
-         ("*\\.component\\.html\\'" . ng2-mode))
-  :config
-  (when (functionp 'prettier-html-on-save-mode)
-    (add-hook 'ng2-mode 'prettier-html-on-save-mode)))
+  :mode (("\\.\\(component\\|service\\|pipe\\|directive\\|guard\\|module\\)\\.ts\\'" . ng2-mode)
+         ("\\.component\\.html\\'" . ng2-mode)))
 
 (provide 'init-web-mode)
 ;;; init-web-mode.el ends here
