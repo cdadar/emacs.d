45e1d8b093e26acec41968d47747a76a770c2c22
cdadar <gunbanmi@gmail.com>
2026-04-29 23:24:26 +0800

    refactor: tidy gud configuration

--- lisp/init-gud.el
+++ lisp/init-gud.el
@@ -1,63 +1,59 @@
+;;; init-gud.el --- Configure GUD debugging helpers -*- lexical-binding: t -*-
+;;; Commentary:
+;;; Code:
+
 (defvar gud-overlay
-  (let* ((ov (make-overlay (point-min) (point-min))))
+  (let ((ov (make-overlay (point-min) (point-min))))
     (overlay-put ov 'face 'secondary-selection)
     ov)
   "Overlay variable for GUD highlighting.")
 
-(defadvice gud-display-line (after my-gud-highlight activate)
-  "Highlight current line."
-  (let* ((ov gud-overlay)
-         (bf (gud-find-file true-file)))
+(defun cdadar/gud-highlight-current-line (true-file line &optional _column)
+  "Highlight LINE in TRUE-FILE after `gud-display-line'."
+  (let ((ov gud-overlay)
+        (bf (gud-find-file true-file)))
     (with-current-buffer bf
-      (move-overlay ov (line-beginning-position) (line-beginning-position 2)
-                    (current-buffer)))))
+      (save-excursion
+        (goto-char (point-min))
+        (forward-line (1- line))
+        (move-overlay ov (line-beginning-position) (line-beginning-position 2)
+                      (current-buffer))))))
 
-(defun gud-kill-buffer ()
-  (if (derived-mode-p 'gud-mode)
-      (delete-overlay gud-overlay)))
-
-(add-hook 'kill-buffer-hook 'gud-kill-buffer)
+(defun cdadar/gud-delete-overlay-on-buffer-kill ()
+  "Delete `gud-overlay' when killing a GUD buffer."
+  (when (derived-mode-p 'gud-mode)
+    (delete-overlay gud-overlay)))
 
 ;; {{ hack buffer
 ;; move the cursor to the end of last line if it's gud-mode
-(defun hack-gud-mode ()
-  (when (string= major-mode "gud-mode")
+(defun cdadar/gud-move-to-buffer-end ()
+  "Move point to the end of the buffer when the current buffer is a GUD buffer."
+  (when (derived-mode-p 'gud-mode)
     (goto-char (point-max))))
 
-(defadvice switch-to-buffer (after switch-to-buffer-after activate)
-  (hack-gud-mode))
-
-(defadvice select-window-by-number (after select-window-by-number-after activate)
-  (hack-gud-mode))
-
-;; windmove-do-window-select is from windmove.el
-(defadvice windmove-do-window-select (after windmove-do-window-select-after activate)
-  (hack-gud-mode))
+(defun cdadar/gud-move-to-buffer-end-after (&rest _)
+  "Run `cdadar/gud-move-to-buffer-end' after window or buffer selection."
+  (cdadar/gud-move-to-buffer-end))
 ;; }}
 
 (defun gud-cls (&optional num)
-  "clear gud screen"
+  "Clear the GUD screen.
+With prefix NUM greater than 1, recenter twice."
   (interactive "p")
   (let ((old-window (selected-window)))
     (save-excursion
       (cond
        ((buffer-live-p (get-buffer "*gud-main*"))
         (select-window (get-buffer-window "*gud-main*"))
-        (end-of-buffer)
+        (goto-char (point-max))
         (recenter-top-bottom)
-        (if (> num 1) (recenter-top-bottom))
+        (when (> num 1)
+          (recenter-top-bottom))
         (select-window old-window))
-       (t (error "GUD buffer doesn't exist!"))
-       ))
-    ))
-
-(use-package gud
-  :ensure nil
-  :config
-  (gud-def gud-kill "kill" "\C-k" "Kill the debugee"))
+       (t (error "GUD buffer doesn't exist!"))))))
 
 (defun gud-kill-yes ()
-  "gud-kill and confirm with y"
+  "Run `gud-kill' in the main GUD buffer and confirm with y."
   (interactive)
   (let ((old-window (selected-window)))
     (save-excursion
@@ -69,10 +65,26 @@
         (comint-send-input)
         (recenter-top-bottom)
         (select-window old-window))
-       (t (error "GUD buffer doesn't exist!"))
-       ))
-    ))
+       (t (error "GUD buffer doesn't exist!"))))))
 
-(global-set-key "\C-x\C-a\C-g" 'gud-run)
+(use-package emacs
+  :ensure nil
+  :hook (kill-buffer . cdadar/gud-delete-overlay-on-buffer-kill)
+  :config
+  (advice-add 'switch-to-buffer :after #'cdadar/gud-move-to-buffer-end-after)
+  (advice-add 'select-window-by-number :after #'cdadar/gud-move-to-buffer-end-after)
+  ;; windmove-do-window-select is from windmove.el
+  (advice-add 'windmove-do-window-select :after #'cdadar/gud-move-to-buffer-end-after))
+
+(use-package gud
+  :ensure nil
+  :bind ("C-x C-a C-g" . gud-run)
+  :config
+  (advice-add 'gud-display-line :after #'cdadar/gud-highlight-current-line)
+  (gud-def gud-kill "kill" "\C-k" "Kill the debugee")
+  ;; `gud' reinitializes the C-x C-a prefix map when loaded, so keep the
+  ;; binding installed after the package is required too.
+  (global-set-key (kbd "C-x C-a C-g") #'gud-run))
 
 (provide 'init-gud)
+;;; init-gud.el ends here
