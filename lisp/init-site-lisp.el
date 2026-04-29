3a21f37c0bcecf3845aa693084656a53e2fcc0b0
cdadar <gunbanmi@gmail.com>
2026-04-29 23:49:07 +0800

    refactor: tidy site-lisp bootstrap

--- lisp/init-site-lisp.el
+++ lisp/init-site-lisp.el
@@ -4,32 +4,36 @@
 
 ;;; Set load path
 
-(require 'cl-lib)
-
-(defun sanityinc/add-subdirs-to-load-path (parent-dir)
-  "Add every non-hidden subdir of PARENT-DIR to `load-path'."
-  (let ((default-directory parent-dir))
+(use-package emacs
+  :ensure nil
+  :preface
+  (defun sanityinc/add-subdirs-to-load-path (parent-dir)
+    "Add every non-hidden subdir of PARENT-DIR to `load-path'."
     (setq load-path
           (append
            (cl-remove-if-not
             #'file-directory-p
-            (directory-files (expand-file-name parent-dir) t "^[^\\.]"))
-           load-path))))
-
-;; Add both site-lisp and its immediate subdirs to `load-path'
-(let ((site-lisp-dir (locate-user-emacs-file "site-lisp/")))
-  (push site-lisp-dir load-path)
-  (sanityinc/add-subdirs-to-load-path site-lisp-dir))
+            (directory-files (expand-file-name parent-dir)
+                             t directory-files-no-dot-files-regexp))
+           load-path)))
+  :config
+  ;; Add both site-lisp and its immediate subdirs to `load-path'
+  (let ((site-lisp-dir (locate-user-emacs-file "site-lisp/")))
+    (add-to-list 'load-path site-lisp-dir)
+    (sanityinc/add-subdirs-to-load-path site-lisp-dir)))
 
 ;;; Utilities for grabbing upstream libs
 
 (defun site-lisp-dir-for (name)
+  "Return the site-lisp directory for library NAME."
   (locate-user-emacs-file (format "site-lisp/%s" name)))
 
 (defun site-lisp-library-el-path (name)
+  "Return the source file path for site-lisp library NAME."
   (expand-file-name (format "%s.el" name) (site-lisp-dir-for name)))
 
 (defun download-site-lisp-module (name url)
+  "Download site-lisp module NAME from URL and return its source path."
   (let ((dir (site-lisp-dir-for name)))
     (message "Downloading %s from %s" name url)
     (unless (file-directory-p dir)
@@ -40,15 +44,15 @@
       el-file)))
 
 (defun ensure-lib-from-url (name url)
+  "Ensure site-lisp library NAME exists locally, downloading from URL if needed."
   (unless (site-lisp-library-loadable-p name)
     (byte-compile-file (download-site-lisp-module name url))))
 
 (defun site-lisp-library-loadable-p (name)
-  "Return whether or not the library `name' can be loaded from a
-source file under ~/.emacs.d/site-lisp/name/"
+  "Return non-nil when library NAME is loadable from `site-lisp'."
   (let ((f (locate-library (symbol-name name))))
-    (and f (string-prefix-p (file-name-as-directory (site-lisp-dir-for name)) f))))
-
+    (and f
+         (string-prefix-p (file-name-as-directory (site-lisp-dir-for name)) f))))
 
 (provide 'init-site-lisp)
 ;;; init-site-lisp.el ends here
