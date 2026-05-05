44477066419b02af92d8be2f2d3b7579c4224484
cdadar <gunbanmi@gmail.com>
2026-05-05 14:36:00 +0800

    refactor: move eat helpers into preface

--- lisp/init-terminals.el
+++ lisp/init-terminals.el
@@ -6,20 +6,18 @@
 
 
 (use-package eat
-  :init
+  :preface
   (defun sanityinc/on-eat-exit (process)
     (when (zerop (process-exit-status process))
       (kill-buffer)
       (unless (eq (selected-window) (next-window))
         (delete-window))))
-  
+
   (defcustom sanityinc/eat-map
     (let ((map (make-sparse-keymap)))
       (define-key map (kbd "t") 'eat-other-window)
       map)
     "Prefix map for commands that create and manipulate eat buffers.")
-
-  (setq-default eat-term-scrollback-size (* 2 1024 1024))
 
   (defun sanityinc/eat-term-get-suitable-term-name (&optional display)
     "Version of `eat-term-get-suitable-term-name' which uses better-known TERM values."
@@ -27,6 +25,8 @@
       (cond ((> colors 8) "xterm-256color")
             ((> colors 1) "xterm-color")
             (t "xterm"))))
+  :init
+  (setq-default eat-term-scrollback-size (* 2 1024 1024))
   (setq eat-term-name 'sanityinc/eat-term-get-suitable-term-name)
 
   :hook (eat-exit . sanityinc/on-eat-exit)
