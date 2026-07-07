fa91576bdfa0d72704dbee51864b44551ffa16ac
Steve Purcell <steve@sanityinc.com>
2026-07-07 14:12:22 +0100

    Disable semantic tokens in eglot

--- lisp/init-eglot.el
+++ lisp/init-eglot.el
@@ -10,6 +10,11 @@
   (eglot-code-action-indicator "✓")
   (eglot-code-action-indications '(eldoc-hint mode-line))
   :config
+  ;; Emacs 31 turns on eglot-semantic-tokens-mode by default, which can be
+  ;; slow and visually noisy; disable it.
+  (defun sanityinc/disable-eglot-semantic-tokens ()
+    (eglot-semantic-tokens-mode -1))
+  (add-hook 'eglot-managed-mode-hook #'sanityinc/disable-eglot-semantic-tokens)
   (use-package consult-eglot))
 
 (provide 'init-eglot)
