8d53eaae242cbbe3254efdfd24bfd9bbde176a88
Steve Purcell <steve@sanityinc.com>
2026-07-07 14:15:34 +0100

    Add expreg

--- lisp/init-editing-utils.el
+++ lisp/init-editing-utils.el
@@ -321,6 +321,10 @@ ORIG is the advised function, which is called with its ARGS."
 
 (advice-add 'kmacro-call-macro :around 'sanityinc/disable-features-during-macro-call)
 
+
+(when (maybe-require-package 'expreg)
+  (global-set-key (kbd "C-=") 'expreg-expand)
+  (global-set-key (kbd "C--") 'expreg-contract))
 
 (use-package imenu-anywhere
   :bind
